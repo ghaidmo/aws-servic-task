7b67a1ee126195c9c350b015b6b06b4e80f2c4f5
ghaid <ghaid_mo_98@outlook.com>
2022-01-18 14:31:05 +0200

    create and print topics

---
+++ create_topics.py
@@ -0,0 +1,8 @@
+from main import client
+response = client.create_topic(
+    Name='MyTopic'
+)
+
+topics_list = client.list_topics()
+for i in topics_list['Topics']:
+    print(i['TopicArn'])
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from urllib import response
 import boto3
 from os import getenv
-
-
 LOCALSTACK_ENDPOINT_URL = getenv("AWS_ENDPOINT_URL")
 
-client = (boto3.client("sns", endpoint_url=LOCALSTACK_ENDPOINT_URL).list_topics())
+client = boto3.client(
+    "sns", endpoint_url=LOCALSTACK_ENDPOINT_URL)
+response = client.create_topic(
+    Name='MyTopic'
+)
+
+topics_list = client.list_topics()
+for i in topics_list['Topics']:
+    print(i['TopicArn'])

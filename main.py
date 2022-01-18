ef97781f54f43398b5ecb06fb286d02a261b9c98
ghaid <ghaid_mo_98@outlook.com>
2022-01-18 11:27:05 +0200

    create new file

--- main.py
+++ main.py
@@ -2,10 +2,7 @@ from urllib import response
 import boto3
 from os import getenv
 
-from websockets import client
 
 LOCALSTACK_ENDPOINT_URL = getenv("AWS_ENDPOINT_URL")
 
-print(getenv('AWS_ACCESS_KEY_ID'))
-
-print(boto3.client("sns", endpoint_url=LOCALSTACK_ENDPOINT_URL).list_topics())
+client = (boto3.client("sns", endpoint_url=LOCALSTACK_ENDPOINT_URL).list_topics())
